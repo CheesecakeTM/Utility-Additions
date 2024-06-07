3e4ea8f0e384830be01a6e5954e9543c78f1ba26
Elisa <acrazypie5@gmail.com>
2024-06-07 21:26:17 +0200

    Initial Commit.

---
+++ src/main/java/net/cheesecake/utilityadditions/UtilityAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.cheesecake.utilityadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UtilityAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cheesecake/utilityadditions/UtilityAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cheesecake.utilityadditions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UtilityAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
