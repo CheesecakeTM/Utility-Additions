cfca20b7ccd8f4e2b71d048da6ca56ae153da8e0
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-06-20 15:27:43 +0200

    Added tooltip for indestructible items.

--- src/main/java/net/cheesecake/utilityadditions/mixin/ItemStackMixin.java
+++ src/main/java/net/cheesecake/utilityadditions/mixin/ItemStackMixin.java
@@ -1,8 +1,13 @@
 package net.cheesecake.utilityadditions.mixin;
 
+import java.util.List;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Language;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -42,6 +47,18 @@ public class ItemStackMixin {
 
         if ( nbt != null && nbt.getBoolean("Indestructible")){
             cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "getTooltip", at = @At("RETURN"))
+    private void getTooltip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
+        ItemStack stack = (ItemStack) (Object) this;
+        List<Text> tooltip = cir.getReturnValue();
+        NbtCompound nbt = stack.getNbt();
+
+        if (nbt != null && nbt.getBoolean("Indestructible")) {
+            // Add custom tooltip "Indestructible" under the item name
+            tooltip.add(Text.of("Indestructible"));
         }
     }
 }
