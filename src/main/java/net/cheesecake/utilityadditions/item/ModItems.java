6e54aa91fc40f98653837f044c5fce7636f10d6d
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-06-25 16:37:39 +0200

    Added new item "Enriched Iron" and the texture of the "Dawn Ore".

--- src/main/java/net/cheesecake/utilityadditions/item/ModItems.java
+++ src/main/java/net/cheesecake/utilityadditions/item/ModItems.java
@@ -13,10 +13,12 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item ETERNALITE = registerItem("eternalite", new Item(new FabricItemSettings()));
     public static final Item DAWNGEM = registerItem("dawngem", new Item(new FabricItemSettings()));
+    public static final Item ENRICHED_IRON = registerItem("enriched_iron", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
         entries.add(ETERNALITE);
         entries.add(DAWNGEM);
+        entries.add(ENRICHED_IRON);
     }
 
     private static Item registerItem(String name, Item item) {
