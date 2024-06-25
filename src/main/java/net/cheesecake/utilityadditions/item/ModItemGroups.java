b1647f5e78606a70c7200a39cfa5694d9b49d69e
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-06-25 16:43:02 +0200

    Added "Enriched Iron" to the mod item group.

--- src/main/java/net/cheesecake/utilityadditions/item/ModItemGroups.java
+++ src/main/java/net/cheesecake/utilityadditions/item/ModItemGroups.java
@@ -17,6 +17,7 @@ public class ModItemGroups {
                     .icon(() -> new ItemStack(ModItems.ETERNALITE)).entries((displayContext, entries) -> {
                         entries.add(ModItems.DAWNGEM);
                         entries.add(ModItems.ETERNALITE);
+                        entries.add(ModItems.ENRICHED_IRON);
 
                         entries.add(ModBlocks.DEEPSLATE_DAWN_ORE);
 
